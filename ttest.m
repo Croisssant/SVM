94828ecc210cba1fdf2d267078a5e9497687e011
bingshan1999 <bingshan1999@gmail.com>
2020-12-18 12:20:16 +0000

    Modified ttest2

--- ttest.m
+++ ttest.m
@@ -1,15 +1,15 @@
 X = [];
 Y = [];
 
-fprintf('Please enter an array of accuracies for algorithm 1 used, separated by commas\n');
+fprintf('Please enter an array of accuracies for algorithm 1 used, in ["value1","value2"] format \n');
 X = input('>>>');
-fprintf('Please enter an array of accuracies for algorithm 2 used, separated by commas\n');
+fprintf('Please enter an array of accuracies for algorithm 2 used, in ["value1","value2"] format \n');
 Y = input('>>>');
 
 diff = ttest2(X,Y);
-if diff == 0
-    fprintf("Algorithm 1 is not statistically significancly different than algorithm 2\n ");
-elseif diff ==1
+if diff == 1
     fprintf("Algorithm 1 is statistically significancly different than algorithm 2\n ");
+else
+    fprintf("Algorithm 1 is not statistically significancly different than algorithm 2\n ");
 end
         
